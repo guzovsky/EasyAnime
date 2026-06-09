19cedcd2a60dee7e33aaee32f6a7e01beb6f5d70
Gustavo Gentil <bluguzera@gmail.com>
2026-06-09 18:20:05 -0400

    refactor(anilist): extract CompoundRouteQuery interface

--- src/lib/anilist/queries/types.ts
+++ src/lib/anilist/queries/types.ts
@@ -5,9 +5,13 @@ import type { TypedDocumentString } from "@/gql/graphql";
 // --------------------------------------------------------
 
 export type RouteQuery = TypedDocumentString<unknown, Record<string, unknown>>;
-export type RouteQueryConfig =
-	| RouteQuery
-	| { card: RouteQuery; banner?: RouteQuery };
+
+export interface CompoundRouteQuery {
+	banner?: RouteQuery;
+	card: RouteQuery;
+}
+
+export type RouteQueryConfig = RouteQuery | CompoundRouteQuery;
 
 // ---------------------
 
