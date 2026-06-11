bd3139a5918133111683a3c57e6898fba8c4096f
Gustavo Gentil <bluguzera@gmail.com>
2026-06-11 14:45:47 -0400

    feat(anilist): add main staff graphql fragments

--- src/gql/graphql.ts
+++ src/gql/graphql.ts
@@ -367,6 +367,10 @@ export type MediaDetailQueryQuery = { Media: { id: number, type: MediaType | nul
 
 export type PageInfoDataFragment = { pageInfo: { total: number | null, perPage: number | null, currentPage: number | null, lastPage: number | null, hasNextPage: boolean | null } | null };
 
+export type StaffCardFragment = { age: number | null, description: string | null, gender: string | null, id: number, primaryOccupations: Array<string | null> | null, image: { large: string | null, medium: string | null } | null, name: { full: string | null, native: string | null } | null };
+
+export type StaffDetailFragment = { age: number | null, id: number, description: string | null, homeTown: string | null, siteUrl: string | null, yearsActive: Array<number | null> | null, gender: string | null, primaryOccupations: Array<string | null> | null, name: { full: string | null, native: string | null, alternative: Array<string | null> | null } | null, image: { large: string | null, medium: string | null } | null, dateOfBirth: { day: number | null, month: number | null, year: number | null } | null, dateOfDeath: { day: number | null, month: number | null, year: number | null } | null, characterMedia: { edges: Array<{ id: number | null, node: { id: number, type: MediaType | null, coverImage: { extraLarge: string | null, large: string | null, medium: string | null } | null, title: { english: string | null, native: string | null, romaji: string | null } | null } | null, characters: Array<{ id: number, name: { full: string | null, native: string | null } | null, image: { large: string | null, medium: string | null } | null } | null> | null } | null> | null } | null };
+
 export class TypedDocumentString<TResult, TVariables>
   extends String
   implements DocumentTypeDecoration<TResult, TVariables>
@@ -668,6 +672,84 @@ export const PageInfoDataFragmentDoc = new TypedDocumentString(`
   }
 }
     `, {"fragmentName":"PageInfoData"}) as unknown as TypedDocumentString<PageInfoDataFragment, unknown>;
+export const StaffCardFragmentDoc = new TypedDocumentString(`
+    fragment StaffCard on Staff {
+  age
+  description(asHtml: true)
+  gender
+  id
+  primaryOccupations
+  image {
+    large
+    medium
+  }
+  name {
+    full
+    native
+  }
+}
+    `, {"fragmentName":"StaffCard"}) as unknown as TypedDocumentString<StaffCardFragment, unknown>;
+export const StaffDetailFragmentDoc = new TypedDocumentString(`
+    fragment StaffDetail on Staff {
+  age
+  id
+  description(asHtml: true)
+  homeTown
+  siteUrl
+  yearsActive
+  gender
+  primaryOccupations
+  name {
+    full
+    native
+    alternative
+  }
+  image {
+    large
+    medium
+  }
+  dateOfBirth {
+    day
+    month
+    year
+  }
+  dateOfDeath {
+    day
+    month
+    year
+  }
+  characterMedia(sort: $characterMediaSort) {
+    edges {
+      id
+      node {
+        id
+        type
+        coverImage {
+          extraLarge
+          large
+          medium
+        }
+        title {
+          english
+          native
+          romaji
+        }
+      }
+      characters {
+        id
+        name {
+          full
+          native
+        }
+        image {
+          large
+          medium
+        }
+      }
+    }
+  }
+}
+    `, {"fragmentName":"StaffDetail"}) as unknown as TypedDocumentString<StaffDetailFragment, unknown>;
 export const MostFavoritedCharactersDocument = new TypedDocumentString(`
     query MostFavoritedCharacters($page: Int, $perPage: Int) {
   Page(page: $page, perPage: $perPage) {
