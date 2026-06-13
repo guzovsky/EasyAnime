748bd5b9f4a18b89a7e082af4d8e0a2032b97000
Gustavo Gentil <bluguzera@gmail.com>
2026-06-13 10:32:59 -0400

    feat(anilist): add QueryType helper type

--- src/lib/anilist/queries/types.ts
+++ src/lib/anilist/queries/types.ts
@@ -26,3 +26,13 @@ export type QueryVariables<T> =
 	T extends TypedDocumentString<unknown, infer V> ? V : never;
 
 // --------------------------------------------------------
+
+export type QueryType<TQuery, TConfig> = TQuery extends CompoundRouteQuery
+	? TConfig extends { dataType: keyof TQuery }
+		? TQuery[TConfig["dataType"]]
+		: never
+	: TQuery extends RouteQuery
+		? TQuery
+		: never;
+
+// --------------------------------------------------------
