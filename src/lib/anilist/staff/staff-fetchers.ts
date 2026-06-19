869c94fd000be0be8c7e2ab92b8456da45568318
Gustavo Gentil <bluguzera@gmail.com>
2026-06-19 11:33:32 -0400

    feat(anilist): add high-level staff fetchers

---
+++ src/lib/anilist/staff/staff-fetchers.ts
@@ -0,0 +1,61 @@
+// --------------------------------------------------------
+
+import type { StaffRouteKey } from "@/config/routes";
+import type {
+	BirthdayStaffsQueryVariables,
+	MostFavoritedStaffsQueryVariables,
+	StaffAllQueryVariables,
+} from "@/gql/graphql";
+import { DEFAULT_PAGINATION_VARIABLES } from "../constants";
+import type { RouteFetchers } from "../types/route-fetchers";
+import { fetchStaff } from "./fetch-staff";
+import type { StaffQueryMap } from "./staff-queries";
+
+// --------------------------------------------------------
+
+const fetchAllStaff = (v: StaffAllQueryVariables = {}) => {
+	const variables = {
+		...DEFAULT_PAGINATION_VARIABLES,
+		...v,
+	} satisfies StaffAllQueryVariables;
+
+	return fetchStaff("all", { variables });
+};
+
+// ---------------------
+
+const fetchBirthdayStaffs = (v: BirthdayStaffsQueryVariables = {}) => {
+	const variables = {
+		...DEFAULT_PAGINATION_VARIABLES,
+		...v,
+	} satisfies BirthdayStaffsQueryVariables;
+
+	return fetchStaff("birthday", { variables });
+};
+
+// ---------------------
+
+const fetchMostFavoritedStaffs = (
+	v: MostFavoritedStaffsQueryVariables = {}
+) => {
+	const variables = {
+		...DEFAULT_PAGINATION_VARIABLES,
+		...v,
+	} satisfies MostFavoritedStaffsQueryVariables;
+
+	return fetchStaff("mostFavorited", { variables });
+};
+
+// --------------------------------------------------------
+
+const _contractCheck = {
+	all: fetchAllStaff,
+	birthday: fetchBirthdayStaffs,
+	mostFavorited: fetchMostFavoritedStaffs,
+} as const satisfies RouteFetchers<StaffRouteKey, StaffQueryMap>;
+
+// --------------------------------------------------------
+
+export { fetchAllStaff, fetchBirthdayStaffs, fetchMostFavoritedStaffs };
+
+// --------------------------------------------------------
