a3c0face9a1fcef8350e019a3e1909646f9f2e20
Gustavo Gentil <bluguzera@gmail.com>
2026-06-23 23:54:38 -0400

    feat: add Poppins font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@
 
 import type { Metadata } from "next";
 import "./globals.css";
+import { Poppins } from "next/font/google";
 import type { ReactNode } from "react";
 
 // --------------------------------------------------------
@@ -13,9 +14,16 @@ export const metadata: Metadata = {
 
 // --------------------------------------------------------
 
+const poppins = Poppins({
+	weight: ["300", "400", "500", "600", "700"],
+	subsets: ["latin"],
+});
+
+// --------------------------------------------------------
+
 export default function RootLayout({ children }: { children: ReactNode }) {
 	return (
-		<html className="antialiased" lang="en">
+		<html className={`${poppins.className} antialiased`} lang="en">
 			<body className="flex h-svh flex-col">
 				<main className="flex flex-1 flex-col">{children}</main>
 			</body>
